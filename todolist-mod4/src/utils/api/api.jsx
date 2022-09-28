d660b8c7015f3e1e8cbbed2d51878c4265d3df8a
Lucas Viccari <lviccari4@gmail.com>
2022-09-28 18:14:52 -0300

    feat: fetch with application on api const

--- todolist-mod4/src/utils/api/api.jsx
+++ todolist-mod4/src/utils/api/api.jsx
@@ -1,12 +1,19 @@
-defaultUrl = "https://animes-api-c017.herokuapp.com/animes";
+const defaultUrl = "https://animes-api-c017.herokuapp.com/animes";
 
-    // export const api = {
-    //     createAnime: async (anime) => {
-    //         fetch( defaultUrl + "/create", {
-    //             method: "POST",
-    //             headers: new Headers ({ "Content-Type": "application/json" }),
-    //             body: JSON.stringify(anime),
+    export const api = {
+        createAnime: async (anime) => {
+            fetch( defaultUrl + "/create", {
+                method: "POST",
+                headers: new Headers ({ "Content-Type": "application/json" }),
+                body: JSON.stringify(anime),
 
-    //         });
-    //     }
-    // }
+            });
+        },
+
+        getAllAnimes: async (anime) => {
+            const response = await fetch( defaultUrl + "/");
+            const allAnimes = await response.json();
+
+            return allAnimes;
+    }
+}
