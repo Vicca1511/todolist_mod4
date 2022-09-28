6237c795cee90f8b1485160fe651af047b6b476e
Lucas Viccari <lviccari4@gmail.com>
2022-09-28 20:03:01 -0300

    feat: including a modal on component Home

--- todolist-mod4/src/pages/Home/Home.jsx
+++ todolist-mod4/src/pages/Home/Home.jsx
@@ -1,14 +1,34 @@
 import "./Home.css";
-import { useState, useEffect } from "react";
 import { Card } from "../.././components/Card/Card";
 import { api } from "../.././utils/api/api";
+import { useState, useEffect } from "react";
+import Modal from "react-modal";
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+   overlay: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+   }
+};
+Modal.setAppElement("#root");
 export function Home() {
   const [animesList, setAnimesList] = useState([]);
+  const [modalIsOpen, setmodalIsOpen] = useState(false);
 
   async function getAnimes() {
     const animes = await api.getAllAnimes();
     setAnimesList(animes);
+  }
+
+  function handleModal() {
+    setmodalIsOpen(!modalIsOpen);
   }
 
   useEffect(() => {
@@ -16,21 +36,36 @@ export function Home() {
   }, []);
 
   return (
-    
-   
+    <>
       <div className="Card__container">
         {animesList.map((item, index) => {
-          return( 
-          <button className="button__card card" key={index} onClick={()=> {console.log(item)}}>
-            <Card 
-            title={item.title} 
-            gender={item.gender} 
-            protagonist={item.protagonist} 
-            year ={item.year} />
-      
-          </button> 
-        )})};
+          return (
+            <button
+              className="button__card card"
+              key={index}
+              onClick={() => handleModal()}
+            >
+              <Card
+                title={item.title}
+                gender={item.gender}
+                protagonist={item.protagonist}
+                year={item.year}
+              />
+            </button>
+          );
+        })}
+        ;
       </div>
-    
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={handleModal}
+        style={customStyles}
+        contentLabel="Card structure"
+      >
+        <section>
+          
+        </section>
+      </Modal>
+    </>
   );
 }
