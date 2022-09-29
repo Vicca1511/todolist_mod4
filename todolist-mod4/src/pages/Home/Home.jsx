85ef3d23bf733ed958e332edad4ee861eab51538
Lucas Viccari <lviccari4@gmail.com>
2022-09-28 21:57:06 -0300

    feat:creating Card Modal

--- todolist-mod4/src/pages/Home/Home.jsx
+++ todolist-mod4/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import "./Home.css";
 import { Card } from "../.././components/Card/Card";
 import { api } from "../.././utils/api/api";
 import { useState, useEffect } from "react";
+import {MdOutlineCloseFullscreen} from "react-icons/md"
 import Modal from "react-modal";
 
 const customStyles = {
@@ -10,17 +11,28 @@ const customStyles = {
     left: "50%",
     right: "auto",
     bottom: "auto",
+    width: "30rem",
+    height: "40rem",
     marginRight: "-50%",
+    paddingTop: "30px",
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
     transform: "translate(-50%, -50%)",
+    textAlign: "center",
+    
+    
   },
-   overlay: {
+ 
+  overlay: {
     backgroundColor: "rgba(0, 0, 0, 0.5)",
-   }
+  },
+
+ 
 };
 Modal.setAppElement("#root");
 export function Home() {
   const [animesList, setAnimesList] = useState([]);
   const [modalIsOpen, setmodalIsOpen] = useState(false);
+  const [oneAnime, setOneAnime] = useState({});
 
   async function getAnimes() {
     const animes = await api.getAllAnimes();
@@ -42,8 +54,11 @@ export function Home() {
           return (
             <button
               className="button__card card"
+              onClick={() => {
+                setOneAnime(item);
+                handleModal();
+              }}
               key={index}
-              onClick={() => handleModal()}
             >
               <Card
                 title={item.title}
@@ -54,7 +69,6 @@ export function Home() {
             </button>
           );
         })}
-        ;
       </div>
       <Modal
         isOpen={modalIsOpen}
@@ -63,7 +77,26 @@ export function Home() {
         contentLabel="Card structure"
       >
         <section>
-          
+          <section style={{
+            display:"flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+            width: "100%"
+          }}>
+            <button style={{
+              cursor:"pointer",
+              backgroundColor: "transparent",
+            }} onClick={handleModal}>
+              <MdOutlineCloseFullscreen size={30} color={" orange "} />
+            </button>
+          </section>
+          <h2>{oneAnime.title}</h2>
+          <ul lassName="card__list">
+            <li className="card__listItem">{oneAnime.protagonist}</li>
+            <li className="card__listItem">{oneAnime.gender}</li>
+            <li className="card__listItem">{oneAnime.year}</li>
+            
+          </ul>
         </section>
       </Modal>
     </>
